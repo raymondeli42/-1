25c3575697bd5c314f57dbfcc0880461a3d07b21
raymondeli42 <raymond.eli42@nwktc.edu>
2015-01-08 13:34:11 -0700

    you just need to do the rest of this haha

---
+++ numberOne/pizza.swift
@@ -0,0 +1,35 @@
+//
+//  pizza.swift
+//  numberOne
+//
+//  Created by Raymond Allen Eli on 1/8/15.
+//  Copyright (c) 2015 Raymond Allen Eli. All rights reserved.
+//
+
+import Foundation
+
+protocol PizzaOrdering
+{
+    func setSize(size: Sizes)
+    func setTopping(topping: Toppings)
+    func address(address:String)
+    func TimeUntilDelivered()->Int
+    
+}
+enum Sizes{
+    case Small
+    
+    case Midium
+    
+    case Large
+}
+
+enum Toppings{
+    case Pepperoni
+    
+    case Mushrooms
+    
+    case Peppers
+    
+    case Sausage
+}
